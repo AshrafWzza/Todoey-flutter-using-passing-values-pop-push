003d2c9aefc8d1d0972717850d1d1583ae5a9f94
AshrafWzza <ashrafwzza2@gmail.com>
2022-09-10 08:28:23 +0200

    Add Dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      // Remove the debug banner
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
